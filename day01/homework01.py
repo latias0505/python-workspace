0333ebc976dc18d3a587cb76051b1d2f1fae714e
latias0505 <latias0505@naver.com>
2023-08-23 11:13:39 +0900

    구구단 과제 교수님 풀이

--- day01/homework01.py
+++ day01/homework01.py
@@ -1,5 +1,26 @@
 import random
 
+num = int(random.randint(20,50))
+print(f'추출된 난수 : {num}')
+
+
+print('< 369게임 시작!!!>')
+for i in range(1, num+1):
+    n1 = i % 10
+    n10 = i // 10
+
+
+    print(f'{i:<3}', end=' ') # i:<3을 사용한 이유는 포멧팅을 할 때 자리수를 3개의 자리를 잡고 왼쪽정렬을 한다는 의미 부등호가 반대면 오른쪽 정렬
+    if not n1:
+        print('뽀'*n10, '숑', end='', sep='')
+    if n1 in [3, 6, 9]:
+        print('짝', end='')
+    if n10 in [3, 6, 9]:
+        print('짝', end='')
+    print()
+
+
+"""
 r = int(random.randint(20,50))
 
 for i in range(1, r+1):
@@ -16,3 +37,4 @@ for i in range(1, r+1):
 
     else:
         print(i)
+"""
